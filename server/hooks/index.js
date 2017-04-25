c50a34ad1ab6aa832df0dbe08b62eee33847b58e
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-04-26 08:31:17 +0900

    Update GPIO

--- server/hooks/index.js
+++ server/hooks/index.js
@@ -4,23 +4,23 @@ import switz    from 'switz'
 
 export default {
   init: () => {
-    exec('echo 23 > /sys/class/gpio/export')
-    exec('echo 24 > /sys/class/gpio/export')
-    exec('echo 25 > /sys/class/gpio/export')
-    exec('echo out > /sys/class/gpio/gpio23/direction')
-    exec('echo out > /sys/class/gpio/gpio24/direction')
-    exec('echo out > /sys/class/gpio/gpio25/direction')
+    exec('echo 14 > /sys/class/gpio/export')
+    exec('echo 15 > /sys/class/gpio/export')
+    exec('echo 18 > /sys/class/gpio/export')
+    exec('echo out > /sys/class/gpio/gpio14/direction')
+    exec('echo out > /sys/class/gpio/gpio15/direction')
+    exec('echo out > /sys/class/gpio/gpio18/direction')
   },
   changeOn : (key, value) => () => {
     switz(key, s => {
-      s.case('toggle0', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio23/value`))
-      s.case('toggle1', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio24/value`))
-      s.case('toggle2', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio25/value`))
+      s.case('toggle0', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio14/value`))
+      s.case('toggle1', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio15/value`))
+      s.case('toggle2', () => exec(`echo ${value ? 1 : 0} > /sys/class/gpio/gpio18/value`))
     })
   },
   terminate: () => {
-    exec('echo 23 > /sys/class/gpio/unexport')
-    exec('echo 24 > /sys/class/gpio/unexport')
-    exec('echo 25 > /sys/class/gpio/unexport')
+    exec('echo 14 > /sys/class/gpio/unexport')
+    exec('echo 15 > /sys/class/gpio/unexport')
+    exec('echo 18 > /sys/class/gpio/unexport')
   },
 }
