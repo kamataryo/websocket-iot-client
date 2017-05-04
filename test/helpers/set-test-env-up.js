0265547e7a326d16300be93bccfd17c7bfaec91a
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-04 18:40:53 +0900

    Set up enzyme and ava testing

---
+++ test/helpers/set-test-env-up.js
@@ -0,0 +1,7 @@
+require('babel-register')
+const { JSDOM } = require('jsdom')
+
+const { window } = new JSDOM('<body></body>')
+global.window = window
+global.document = window.document
+global.navigator = window.navigator
